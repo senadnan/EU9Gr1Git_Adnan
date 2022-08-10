8de998597d3b24c4f381b96a82022db3ac3c8291
refikturk <rfkturk43@gmail.com>
2022-08-10 21:14:30 +0300

    second commit from Refik

--- src/Refik.java
+++ src/Refik.java
@@ -2,5 +2,6 @@ public class Refik {
     public static void main(String[] args) {
 
         System.out.println("First commit from Refik branch");
+        System.out.println("Second commit from Refik branch");
     }
 }
