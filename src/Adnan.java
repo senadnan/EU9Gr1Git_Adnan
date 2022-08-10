8ab39b52762baffa8258ccef8ee5eaf5d3b5c444
Adnan Sen <senadnan82@gmail.com>
2022-08-10 20:46:55 +0300

    second commit from Adnan

--- src/Adnan.java
+++ src/Adnan.java
@@ -2,6 +2,7 @@ public class Adnan {
     public static void main(String[] args) {
 
         System.out.println("First change from Adnan");
+        System.out.println("Second change from Adnan");
 
     }
 
