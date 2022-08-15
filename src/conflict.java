d85f7e4d4d0634bb38d1b010291fce81bfd693b1
Adnan Sen <senadnan82@gmail.com>
2022-08-15 20:20:46 +0300

    second commit from Adnan

---
+++ src/conflict.java
@@ -0,0 +1,17 @@
+public class conflict {
+    public static void main(String[] args) {
+        // codes from Adnan
+        // codes from Adnan
+        // codes from Adnan
+        // codes from Adnan
+        // codes from Adnan
+        // codes from Adnan
+        // codes from Adnan
+        // codes from Adnan
+        // codes from Adnan
+        // codes from Adnan
+
+
+
+    }
+}
