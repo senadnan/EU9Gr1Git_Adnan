23b5bb0d67a528fbc8f2a2b11121ad5bb30f5e07
mziya <mziyag5187@gmail.com>
2022-08-15 20:35:26 +0300

    commit from Ziya branch to create merge conflict

---
+++ src/conflict.java
@@ -0,0 +1,14 @@
+public class conflict {
+    public static void main(String[] args) {
+        //codes from Ziya for creating conflict
+        //codes from Ziya for creating conflict
+        //codes from Ziya for creating conflict
+        //codes from Ziya for creating conflict
+        //codes from Ziya for creating conflict
+        //codes from Ziya for creating conflict
+        //codes from Ziya for creating conflict
+        //codes from Ziya for creating conflict
+        //codes from Ziya for creating conflict
+        //codes from Ziya for creating conflict
+    }
+}
